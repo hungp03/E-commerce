5b828b810308776824fd9983e4e07ac8bed6a254
hungp03 <ph.hung0903@gmail.com>
2023-08-19 23:14:29 +0700

    RF token update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,11 +1,11 @@
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
 const {
   generateAccessToken,
   generateRefreshToken,
 } = require("../middleware/jwt");
 
-//Register
 const register = asyncHandler(async (req, res) => {
   const { email, password, firstname, lastname } = req.body;
   //kiểm tra trước để giảm tải cho db
@@ -26,7 +26,7 @@ const register = asyncHandler(async (req, res) => {
   }
 });
 
-//Login
+
 //RefreshToken: cấp mới AccessToken
 //AccessToken: Xác thực, phân quyền người dùng
 const login = asyncHandler(async (req, res) => {
@@ -69,14 +69,70 @@ const login = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   const { _id } = req.user;
   //Dùng select trong mongo để chọn những field cần lấy, thêm dấu '-' để ẩn
-  const user = await User.findById(_id).select('-refreshToken -password -role');
+  const user = await User.findById(_id).select("-refreshToken -password -role");
   return res.status(200).json({
     success: false,
     result: user ? user : "user not found",
   });
 });
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  //Lấy token từ cookies
+  const cookie = req.cookies;
+  //Kiểm tra xem có RF token hay không
+  if (!cookie && !cookie.refreshToken)
+    throw new Error("No RF token in cookies");
+  //Kiểm tra RF token có hợp lệ hay không
+  const result = await jwt.verify(cookie.refreshToken, process.env.JWT_SECRET);
+  const response = await User.findOne({
+    _id: result._id,
+    refreshToken: cookie.refreshToken,
+  });
+  res.status(200).json({
+    success: response ? true : false,
+    newAccessToken: response
+      ? generateAccessToken(response._id, response.role)
+      : "Refresh token not matched",
+  });
+  //Đoạn code dưới không thể throw Error khi RF token hết hạn
+  /* jwt.verify(cookie.refreshToken, process.env.JWT_SECRET, async (err, decode) => {
+      if (err) throw new Error("Invalid RF token");
+      //Check xem có trùng RF token với DB không
+      const response = await User.findOne({_id: decode._id, refreshToken: cookie.refreshToken,});
+      res.status(200).json({
+        success: response ? true : false,
+        newAccessToken: response ? generateAccessToken(response._id, response.role) : "Refresh token not matched",
+      });
+    });*/
+});
+
+const logout = asyncHandler(async (req, res) => {
+  //Lấy cookies
+  const cookie = req.cookies;
+  //Kiểm tra có RF token hay không (đang ở trạng thái đăng nhập hay không)
+  if (!cookie || !cookie.refreshToken)
+    throw new Error("No RF token in cookies");
+  //Xóa RF token trong DB trùng với RF token trên browser
+  await User.findOneAndUpdate(
+    { refreshToken: cookie.refreshToken },
+    { refreshToken: "" },
+    { new: true }
+  );
+  //Xóa RF token trong cookies của browser
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: true,
+  });
+  return res.status(200).json({
+    success: true,
+    message: 'Logout done'
+  })
+});
+
 module.exports = {
   register,
   login,
   getCurrentUser,
+  refreshAccessToken,
+  logout,
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const ctrls = require("../controllers/user");
 const { verifyAccessToken } = require("../middleware/verifyToken");
+
 router.post("/register", ctrls.register);
 router.post("/login", ctrls.login);
 router.get("/current", verifyAccessToken, ctrls.getCurrentUser);
+router.post("/refreshToken", ctrls.refreshAccessToken);
+router.post("/logout", ctrls.logout);
 
 module.exports = router;
