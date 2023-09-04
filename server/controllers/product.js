cc8646d189ab93a7997ab20d2608be804402c79e
hungp03 <ph.hung0903@gmail.com>
2023-09-04 16:10:08 +0700

    Filter + sorting product

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const Product = require("../models/product");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
@@ -26,12 +27,57 @@ const getProduct = asyncHandler(async (req, res) => {
 });
 
 //Filtering, sorting and oagination
+//https://jeffdevslife.com/p/1-mongodb-query-of-advanced-filtering-sorting-limit-field-and-pagination-with-mongoose/
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
-  return res.status(200).json({
-    success: products ? true : false,
-    products: products ? products : "No data",
-  });
+  const queries = { ...req.query };
+  //tách các trường đặc biệt ra khỏi query
+  const excludeFields = ["limit", "sort", "page", "fields"];
+  excludeFields.forEach((el) => delete queries[el]);
+  //Format lại các operators cho đúng cú pháp Mongoose
+  let queryString = JSON.stringify(queries);
+  queryString = queryString.replace(
+    /\b(gt|gte|lt|lte)\b/g,
+    (matchedElement) => `$${matchedElement}`
+  );
+  const formatedQueries = JSON.parse(queryString);
+
+  //Filtering
+  if (queries?.title)
+    formatedQueries.title = { $regex: queries.title, $options: "i" };
+  //không dùng await => pending để xử lý các lệnh
+  let queryCommand = Product.find(formatedQueries);
+
+  //Sorting
+  if (req.query.sort){
+    //Split: abc,efg => [abc, efg] => join => [abc efg]
+    const sortBy = req.query.sort.split(',').join(' ')
+    queryCommand = queryCommand.sort(sortBy)
+  }
+  //Moongoose 7 trở lên không hỗ trợ callback function trong exec()
+  /*queryCommand.exec(async(err, response)=>{
+    if (err) throw new Error(err.message);
+    const counts = await Product.find(formatedQueries).countDocuments();
+    return res.status(200).json({
+      success: products ? true : false,
+      products: products ? products : "No data",
+      counts 
+    });
+  })
+  */
+  //excute query
+  //số lượng sp thỏa điều kiện !== số lượng sp mà API gọi ra
+  queryCommand
+    .then(async (response) => {
+      const counts = await Product.find(formatedQueries).countDocuments();
+      return res.status(200).json({
+        success: counts ? true : false,
+        products: response.length ? response : "No data",
+        counts,
+      });
+    })
+    .catch((err) => {
+      throw new Error(err.message);
+    });
 });
 
 const updateProduct = asyncHandler(async (req, res) => {
@@ -64,5 +110,5 @@ module.exports = {
   getProduct,
   getProducts,
   updateProduct,
-  deleteProduct
+  deleteProduct,
 };
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -74,9 +74,9 @@ const login = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   const { _id } = req.user;
   //Dùng select trong mongo để chọn những field cần lấy, thêm dấu '-' để ẩn
-  const user = await User.findById(_id).select("-refreshToken -password -role");
+  const user = await User.findById(_id).select("-refreshToken -password -role -passwordResetExpires -passwordResetToken");
   return res.status(200).json({
-    success: false,
+    success: user? true: false,
     result: user ? user : "user not found",
   });
 });
