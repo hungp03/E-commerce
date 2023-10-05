68a54205075d2de0c276b7f08f6219fea4d5f007
hungp03 <ph.hung0903@gmail.com>
2023-10-05 23:47:00 +0700

    config cloudinary

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,14 +4,16 @@ const { notFound, errHandler } = require("../middleware/errorHandler");
 const productCategoryRouter = require("./productCategory");
 const blogCategoryRouter = require("./blogCategory");
 const blogRouter = require("./blog");
-const brandRouter = require('./brand')
+const brandRouter = require("./brand");
+const couponRouter = require("./coupon");
 const initRoutes = (app) => {
   app.use("/api/user", userRouter);
   app.use("/api/product", productRouter);
   app.use("/api/productcategory", productCategoryRouter);
   app.use("/api/blogcategory", blogCategoryRouter);
   app.use("/api/blog", blogRouter);
-  app.use('/api/brand', brandRouter)
+  app.use("/api/brand", brandRouter);
+  app.use("/api/coupon", couponRouter);
   //Pass the error not found
   app.use(notFound);
   //Handler error
